1cf0402bfe1ecf24b3fe013a4dfcd6b89f43945c
tfmontague <156025728+tfmontague@users.noreply.github.com>
2024-01-11 20:23:21 -0600

    Update mont_analytics_utils.py

--- mont_analytics_utils.py
+++ mont_analytics_utils.py
@@ -1,1 +1,16 @@
+''' This module provides a reusable byline for Montague Analytics' services. '''
 
+#importing dependencies
+import math
+import statistics
+
+
+#defining variables
+business_name: str = "Montague Analytics, LLC"
+count_completed_projects: int = 3
+is_veteran_owned: bool = True
+average_google_rating: float = 4.7
+project_types: list = ["Data Analysis", "Data Strategy", "Data Platform Solution Design", "Business Analysis", "Data Governance Consulting"]
+google_rating_scores: list = [4.4, 5.0, 4.8]
+
+#defining formatted strings
